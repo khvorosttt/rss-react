5fa465864a1c3030128990eca76571d49eaffc22
khvorosttt <khvorosttt@yandex.by>
2024-07-07 21:37:14 +0300

    feat: add a function to interact with the API

---
+++ src/services/api/Api.ts
@@ -0,0 +1,49 @@
+interface AnimalBody {
+    uid: string;
+    name: string;
+    earthAnimal: boolean;
+    earthInsect: boolean;
+    avian: boolean;
+    canine: boolean;
+    feline: boolean;
+}
+
+interface PageInfo {
+    pageNumber: number;
+    pageSize: number;
+    numberOfElements: number;
+    totalElements: number;
+    totalPages: number;
+    firstPage: boolean;
+    lastPage: boolean;
+}
+
+interface ClausesInfo {
+    name: string;
+    direction: 'ASC' | 'DESC';
+    clauseOrder: number;
+}
+
+export interface ResponseBody {
+    page: PageInfo;
+    sort: ClausesInfo;
+    animals: AnimalBody[];
+}
+
+const POSTOptions = (request: string) => {
+    return {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `name=${request}`,
+    };
+};
+
+export default class Api {
+    static async fetchData(animalName: string) {
+        return fetch(`https://stapi.co/api/v1/rest/animal/search`, POSTOptions(animalName)).then((response) =>
+            response.json()
+        );
+    }
+}
