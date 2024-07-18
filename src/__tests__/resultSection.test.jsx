c1ffb801f5cdf47ada7733ac83cc5b6e8317be2e
khvorosttt <khvorosttt@yandex.by>
2024-07-18 20:44:16 +0300

    test: test resultSection component

---
+++ src/__tests__/resultSection.test.jsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { BrowserRouter } from 'react-router-dom';
+import { testAnimals } from './data';
+import ResultSection from '../components/ResultSection/ResultSection';
+
+describe('test result section component', () => {
+    it(`should display ${testAnimals.length} cards`, () => {
+        const { container } = render(
+            <BrowserRouter>
+                <ResultSection result={testAnimals} isLoading={false} />
+            </BrowserRouter>
+        );
+        expect(container.querySelectorAll('.card').length).toBe(testAnimals.length);
+    });
+
+    it('should display empty message', () => {
+        render(
+            <BrowserRouter>
+                <ResultSection result={[]} isLoading={false} />
+            </BrowserRouter>
+        );
+        expect(screen.getByText('No results were found for your request')).toBeInTheDocument();
+    });
+});
